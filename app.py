e4e704326822c8ad0931075c1f18955dc50994c7
Uttam Kumar <uk89197@gmail.com>
2025-07-22 00:38:51 +0530

    Deployment code changes

--- app.py
+++ app.py
@@ -2431,4 +2431,4 @@ if not users_collection.find_one({"role": "admin"}):
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=False)
+    socketio.run(app, host="0.0.0.0", port=port, debug=False , server='eventlet')
